9653dcfe1c2db0c872f8826e9b027e187943a888
Rohit <stylishrb711@gmail.com>
2022-08-03 23:06:00 +0530

    bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>React Todo App</h1>
+      <div className="main-div">
+        <div className="card-section">
+          <div className="card-heading">Todo App</div>
+          <div className="card-input my-2">
+            <input
+              className="form-control w-75"
+              type="text"
+              name="task"
+              placeholder="Enter task"
+            />
+            <button className="btn btn-primary mx-2 w-25">Add</button>
+          </div>
+          <div className="card-content">
+            <div className="row">
+              <div className="col-8">Buy Mango</div>
+              <div className="col-4">
+                <div className="btn btn-danger mx-3">Delete</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
