84560c671fed7e4092afcc4523b7de5665deface
Rohit <stylishrb711@gmail.com>
2022-09-30 17:03:50 +0530

    app update!

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import Todo from "./Components/Todo";
 
 function App() {
   return (
-    <div className="App">
-      <div className="main-div">
-        <div className="card-section">
-          <div className="card-heading">Todo App</div>
-          <div className="card-input my-2">
-            <input
-              className="form-control w-75"
-              type="text"
-              name="task"
-              placeholder="Enter task"
-            />
-            <button className="btn btn-primary mx-2 w-25">Add</button>
-          </div>
-          <div className="card-content">
-            <div className="row my-3 mx-2">
-              <div className="col-8">Buy Mango</div>
-              <div className="col-4">
-                <div className="btn btn-danger mx-3">Delete</div>
-              </div>
-            </div>
-            <div className="row my-3 mx-2">
-              <div className="col-8">Buy Banana</div>
-              <div className="col-4">
-                <div className="btn btn-danger mx-3">Delete</div>
-              </div>
-            </div>
-            <div className="row my-3 mx-2">
-              <div className="col-8">Buy Apple</div>
-              <div className="col-4">
-                <div className="btn btn-danger mx-3">Delete</div>
-              </div>
-            </div>
-            <div className="row my-3 mx-2">
-              <div className="col-8">Buy Fruits</div>
-              <div className="col-4">
-                <div className="btn btn-danger mx-3">Delete</div>
-              </div>
-            </div>
-            <div className="row my-3 mx-2">
-              <div className="col-8">Buy Shakes</div>
-              <div className="col-4">
-                <div className="btn btn-danger mx-3">Delete</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Todo />
     </div>
   );
 }
---
+++ src/Components/Todo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+
+const Todo = () => {
+  return (
+    <div className="main-div">
+      <div className="input-card">
+        {/* <input type="text" name="task" id="task" className="form-control" />
+        <AiOutlinePlus /> */}
+      </div>
+      <div className="content-part"></div>
+    </div>
+  );
+};
+
+export default Todo;
